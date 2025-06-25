81686ada33c4a5e7cb3740af866caea0df98cf2e
Sourav Paul <souravpaul.1507@gmail.com>
2025-06-26 00:43:47 +0530

    Added coin table UI with cache

--- src/cointable/cointable.jsx
+++ src/cointable/cointable.jsx
@@ -17,7 +17,64 @@ function Cointable() {
   if (isError){
      return <div>Error: {error.message}</div>
   }
-  return <div>Coin Table <button onClick={()=>setPage(page+1)}>Click me!</button>{page}</div>;
+
+  return(
+    <div className="my-5 flex flex-col items-center justify-center gap-5 w-[80vw] mx-auto">
+      <div className="w-full bg-yellow-400 text-black flex py-3 px-3 font-semibold items-center      justify-center">
+          {/* header of coin table */}
+          <div className="basis-[35%]">Coin</div>
+          <div className="basis-[25%]">Price</div>
+          <div className="basis-[20%]">24 hrs cahnge value</div>
+          <div className="basis-[20%]">Market Cap</div>
+      </div>
+      <div className="flex flex-col w-[80vw] mx-auto">
+        {data && data.map((coin) => {
+          return (
+            <div key={coin.id} className="w-full background-transparent text-black flex py-4 px-2 font-semibold items-center justify-between">
+              <div className="flex items-center justify-start gap-3 basis-[35%]">
+                  <div className="w-[5rem] h-[5rem]">
+                       <img src={coin.image} className="w-full h-full"/>
+                  </div>
+                  <div className="flex flex-col"> 
+                    <div className="text-3xl"> {coin.name}</div>
+                    <div className="text-xl"> {coin.symbol}</div>
+                  </div>
+               </div>
+               <div className="basis-[25%]">
+                 {coin.current_price}
+               </div>
+               <div className="basis-[20%]">
+                 {coin.high_24h}
+               </div>
+               <div className="basis-[20%]">
+                 {coin.market_cap}
+               </div>
+
+            </div>
+          );
+        })}
+
+      </div>
+
+      <div className="flex gap-4 justify-center items-center">
+        <button 
+           disabled={page === 1}
+           onClick={()=>setPage(page-1)} 
+           className="btn btn-primary btn-wide text-black text-2xl"
+        > Prev
+        </button>
+        <button 
+          onClick={()=>setPage(page+1)} 
+          className="btn btn-secondary btn-wide text-black text-2xl"
+        >
+          Next
+        </button>
+      </div>
+
+
+    </div>
+  )
+  
 }
 
 export default Cointable;
