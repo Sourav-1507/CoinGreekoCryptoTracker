d191f4df7909372aa88d3424dfef2823d0e61af0
Sourav Paul <souravpaul.1507@gmail.com>
2025-07-29 00:49:06 +0530

    added Error Boundary

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -2,25 +2,19 @@ import banner from '../../assets/banner2.jpg';
 function Banner(){
   return(
     <div className='mt-6 w-full h-[20rem] relative '>
-        <img src={banner} alt="Logo" className='w-full h-full'/>
-
+        <img src={banner} alt="Logo" className='w-full h-full' />
         <div className='absolute top-20 left-0 right-0 mx-auto w-[20rem]'>
           <div className='flex flex-col gap-4'>
              <div className='font-semibold text-5xl text-white'>
                 Crypto Tracker
-
              </div>
              <div className='font-semibold text-sm text-white text-center'>
-                  Get all info
-
-             </div>
+                  Get all inf
+              </div>
           </div>
-
-
         </div>
-
-
     </div>
+    
     
   )
 }
--- src/Component/Router/Routing.jsx
+++ src/Component/Router/Routing.jsx
@@ -3,28 +3,27 @@ import { Route, Routes } from 'react-router-dom'
 import { Facebook } from 'react-content-loader'
 import {lazy,Suspense} from 'react'
 import LayOut from '../../Pages/LayOut'
+import CustomErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+
 
 const Home = lazy(()=>import ('../../Pages/home') );
 const CoinDetailsPage = lazy(()=>import ('../../Pages/CoinDetailsPage') );
 
+
+
 const Routing = () => {
   return (
-    
-       
-          <Routes>
-             <Route path= "/" element={<LayOut />}>
-                <Route index element ={<Home />} />
-                <Route path= "/details/:coinId" element= {
-                  <Suspense fallback={<div><Facebook /></div>}>
-                    <CoinDetailsPage />
-                  </Suspense> }
- 
-                    
-                />
-            </Route>
-          </Routes>
-        
+    <CustomErrorBoundary>
+      <Suspense fallback={<div><Facebook /></div>}>
+        <Routes>
+          <Route path="/" element={<LayOut />}>
+            <Route index element={<Home />} />
+            <Route path="/details/:coinId" element={<CoinDetailsPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </CustomErrorBoundary>
   );
 };
 
-export default Routing
+export default Routing;
